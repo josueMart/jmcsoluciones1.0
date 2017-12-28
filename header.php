11ec255827bc18e7a5c4799cdb8b0b3ec7246b8e
Josue Martinez <josue.mart@live.com.mx>
2017-12-28 10:06:01 -0600

    Agrego los enlaces al style.css

--- header.php
+++ header.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 	<title>JMC Soluciones | Home</title>
 </head>
 <body>
-	<h1>Este es el header</h1>
+	<h1 id="tHeader">Este es el header</h1>
