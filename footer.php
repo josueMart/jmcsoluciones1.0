f05dca2df09b883c495f4516c5493bb031503e6b
Josue Martinez <josue.mart@live.com.mx>
2017-12-28 17:44:46 -0600

    Agrego footer con diseño básico

--- footer.php
+++ footer.php
@@ -1,5 +1,33 @@
-<h2>Este es el footer</h2>	
+<footer>
+	<div class="container">
+		<div class="row row-footer">
+			<div class="col-4 col-footer">
+				<h2>INFORMACIÓN DE LA EMPRESA</h2>
+				<a class="link-footer" href="">¿Quienes Somos?</a>
+			</div>
+			<div class="col-4 col-footer">
+				<h2>PROMOCIONES</h2>
+				<a class="link-footer" href="">Ofertas del mes</a>
+			</div>
+			<div class="col-4 col-footer">
+				<h2>CONTÁCTANOS</h2>
+				<a class="link-footer-a" href=""><span class="fab fa-twitter link-footer-icon"></span></a>
+				<a class="link-footer-a" href=""><span class="fab fa-facebook link-footer-icon"></span></a>
+				<a class="link-footer-a" href=""><span class="far fa-envelope link-footer-icon"></span></a>
 
 
+				<span></span>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<p>Sitio Desarrollado por JosueMC</p>
+			</div>
+		</div>
+	</div>
+</footer>
+ <?php wp_footer(); ?>
+
+		
 </body>
 </html>
