589cb9d45777f6c2159d678042cecc0da6ba4614
Josue Martinez <josue.mart@live.com.mx>
2017-12-29 17:50:03 -0600

    Queda terminado la estructura general del Index.php

--- index.php
+++ index.php
@@ -1,35 +1,319 @@
 <?php get_header(); ?>
 <!-- SlidePromos -->
 <div class="container">
-	<div class="row">
-		<div class="col">
-			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="" alt="First slide">
+ <div class="row">
+  <div class="col">
+   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100  slide-img" src="http://localhost:8888/wp-content/uploads/2017/12/slide2.png" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 slide-img" src="http://localhost:8888/wp-content/uploads/2017/12/slide2.png" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 slide-img" src="http://localhost:8888/wp-content/uploads/2017/12/slide2.png" alt="Third slide">
+      </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="" alt="Third slide">
-    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
 </div>
-		</div>
-	</div>
+</div>
+<!-- Todo el contenido debe de ir dentro de columnas -->
+      <!-- Y las columnas deben de ir directamente dentro de las filas  -->
+
+      <!-- Nuevo: 
+
+      .col      Extra Pequeño (Extra small)   - Samartphones vertical   Menos de 544px
+      .col-sm   Pequeño(Small)                - Smartphones Vertical    Mas de 544px y menos de 768px
+      .col-md   Mediano (medium)              - Tablets                 Mas de 768px y menos de 992px
+      .col-lg   Largo (Large)                 - Computadoras            Mas de 992px y menos de 1200px
+      .col-xl   Extra Largo (Extra large)     - Computadoras            Mas de 1200px -->
+<!-- Seccion OFERTAS -->
+<div class="row justify-content-around" id="access-direct-row">
+  <div class="col-lg-2 col-md-3 col-sm-6 access-direct ">
+    <a href="" class="direct-link">
+      <div class="row">
+      <img class="img-access-direct" src="http://localhost:8888/wp-content/uploads/2017/12/laptop.jpg" alt="">
+    </div>
+    <div class="row name-access-direct">
+      <div class="col column-name-direct">
+        <p class="">Laptops</p>
+      </div>
+    </div>
+    </a>
+  </div>
+<div class="col-lg-2 col-md-3 col-sm-6 access-direct ">
+    <a href="" class="direct-link">
+      <div class="row">
+      <img class="img-access-direct" src="http://localhost:8888/wp-content/uploads/2017/12/laptop.jpg" alt="">
+    </div>
+    <div class="row name-access-direct">
+      <div class="col column-name-direct">
+        <p class="">Laptops</p>
+      </div>
+    </div>
+    </a>
+  </div>
+  <div class="col-lg-2 col-md-3 col-sm-6 access-direct ">
+    <a href="" class="direct-link">
+      <div class="row">
+      <img class="img-access-direct" src="http://localhost:8888/wp-content/uploads/2017/12/laptop.jpg" alt="">
+    </div>
+    <div class="row name-access-direct">
+      <div class="col column-name-direct">
+        <p class="">Laptops</p>
+      </div>
+    </div>
+    </a>
+  </div>
+  <div class="col-lg-2 col-md-3 col-sm-6 access-direct ">
+    <a href="" class="direct-link">
+      <div class="row">
+      <img class="img-access-direct" src="http://localhost:8888/wp-content/uploads/2017/12/laptop.jpg" alt="">
+    </div>
+    <div class="row name-access-direct">
+      <div class="col column-name-direct">
+        <p class="">Laptops</p>
+      </div>
+    </div>
+    </a>
+  </div>
+  <div class="col-lg-2 col-md-3 col-sm-6 access-direct ">
+    <a href="" class="direct-link">
+      <div class="row">
+      <img class="img-access-direct" src="http://localhost:8888/wp-content/uploads/2017/12/laptop.jpg" alt="">
+    </div>
+    <div class="row name-access-direct">
+      <div class="col column-name-direct">
+        <p class="">Laptops</p>
+      </div>
+    </div>
+    </a>
+  </div>
+
+
+</div>
+<!-- Principales ofertas -->
+
+<div class="row justify-content-around">
+  <div class="col-lg-3">
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+  </div>
+  <div class="col-lg-3">
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+  </div>
+  <div class="col-lg-3">
+    <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="..." alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+  <div class="card-body">
+    <a href="#" class="card-link">Card link</a>
+    <a href="#" class="card-link">Another link</a>
+  </div>
+</div>
+  </div>
+
+
+
+</div>
+<!-- Carrousel Parts -->
+<div class="row">
+  <div class="col">
+   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100  slide-img" src="http://localhost:8888/wp-content/uploads/2017/12/parts1.png" alt="First slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 slide-img" src="http://localhost:8888/wp-content/uploads/2017/12/parts2.png" alt="Second slide">
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100 slide-img" src="http://localhost:8888/wp-content/uploads/2017/12/parts3.png" alt="Third slide">
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</div>
+<!-- Principales Clientes -->
+
+<section class="costumers">
+  <p id="principales-marcas">Nuestras principales marcas</p>
+  <div class="row justify-content-around">
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/xerox.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/xbox.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/western.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/toshiba.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/sony.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/samsung.png" alt="" class="convert-brand-img"> 
+  </div>
+
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/philips.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/perfect-choice.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/nvidia.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/microsoft.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/manhattan.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/logitech.png" alt="" class="thumbnail"> 
+  </div>
+</div>
+<div class="row justify-content-around">
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/lg.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/lenovo.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/lanix.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/kingston.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/kaspersky.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/jvc.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/intel.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/hp.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/ghia.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/gateway.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/epson.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/dell.png" alt="" class="thumbnail"> 
+  </div>
+</div>
+<div class="row justify-content-around">
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/cisco.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/canon.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/brother.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/ati.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/aspel.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/apple.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/amd.png" alt="" class="thumbnail"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/adata.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/acteck.png" alt="" class="convert-brand-img"> 
+  </div>
+  <div class="col-lg-1">
+    <img src="http://localhost:8888/wp-content/uploads/2017/12/acer.png" alt="" class="convert-brand-img"> 
+  </div>
+</div>
+</section>
+
+
+
 </div>
 <?php get_footer(); ?>
